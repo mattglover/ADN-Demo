16589097458c0cddfe165fc38c73424d45b0a03d
Matt Glover <matt@duchysoftware.com>
2013-02-05 11:22:37 +0000

    Initial Commit

---
+++ ADNDemoTests/ADNDemoTests.h
@@ -0,0 +1,13 @@
+//
+//  ADNDemoTests.h
+//  ADNDemoTests
+//
+//  Created by Matt Glover on 05/02/2013.
+//  Copyright (c) 2013 Duchy Software Ltd. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ADNDemoTests : SenTestCase
+
+@end
---
+++ ADNDemoTests/ADNDemoTests.m
@@ -0,0 +1,32 @@
+//
+//  ADNDemoTests.m
+//  ADNDemoTests
+//
+//  Created by Matt Glover on 05/02/2013.
+//  Copyright (c) 2013 Duchy Software Ltd. All rights reserved.
+//
+
+#import "ADNDemoTests.h"
+
+@implementation ADNDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ADNDemoTests");
+}
+
+@end
